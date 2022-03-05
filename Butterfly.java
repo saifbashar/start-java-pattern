6cc97f521e9505c9a88d83f2502070acc2ec5892
saifbashar <saifbashar2021@gmail.com>
2022-03-05 15:31:37 +0600

    Add a pattern

---
+++ Butterfly.java
@@ -0,0 +1,39 @@
+package com.company.saifbashar;
+import java.util.*;
+
+public class Main {
+//    static final double pi = 3.1416;
+    public static void main(String[] args) {
+      int row = 4;
+      for(int i=1;i<=row;i++){
+          for(int j=1;j<=i;j++){
+              System.out.print("*");
+          }
+          for(int j=row;j>i;j--){
+              System.out.print(" ");
+          }
+          for(int j=row;j>i;j--){
+              System.out.print(" ");
+          }
+          for(int j=1;j<=i;j++) {
+              System.out.print("*");
+          }
+          System.out.println();
+      }
+      for(int i=1;i<=row;i++){
+          for(int j = row;j>=i;j--){
+              System.out.print("*");
+          }
+          for(int j=1;j<i;j++){
+              System.out.print(" ");
+          }
+          for(int j=1;j<i;j++){
+              System.out.print(" ");
+          }
+          for(int j = row;j>=i;j--){
+              System.out.print("*");
+          }
+          System.out.println();
+      }
+    }
+}
